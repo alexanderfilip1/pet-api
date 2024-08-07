f8db406cb2cfe5384c8301cec020be16b46e8c0c
alexanderfilip1 <showgammer@gmail.com>
2024-08-07 20:31:00 +0300

    Implemented header and mobile header design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import DocumentationPage from "./pages/DocumentationPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/documentation" element={<DocumentationPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Header.css";
+import MobileHeader from "./MobileHeader";
+import "../Links";
 
 export default function Header() {
   const links = [
     {
-      path: "Register",
-      url: "/register",
+      path: "Documentation",
+      url: "/documentation",
     },
     {
       path: "Sign in",
       url: "/login",
     },
     {
-      path: "Documentation",
-      url: "/documentation",
+      path: "Register",
+      url: "/register",
     },
   ];
+
+  const [burgerMenu, setBurgetMenu] = useState(false);
 
   return (
     <>
@@ -36,6 +40,41 @@ export default function Header() {
               );
             })}
           </ul>
+
+          <div
+            className="header__navbar--burgerMenu"
+            onClick={() => {
+              setBurgetMenu(!burgerMenu);
+            }}
+          >
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 18L20 18"
+                stroke="#fff"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+              <path
+                d="M4 12L20 12"
+                stroke="#fff"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+              <path
+                d="M4 6L20 6"
+                stroke="#fff"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+            </svg>
+          </div>
+          {burgerMenu && <MobileHeader />}
         </nav>
       </header>
     </>
