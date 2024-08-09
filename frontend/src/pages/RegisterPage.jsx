cef566b4c2bd7eea94424aa034bf46d13771bfcc
alexanderfilip1 <showgammer@gmail.com>
2024-08-09 12:38:59 +0300

    Implemented preloader for register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import Header from "../components/Header";
 import "../assets/css/Register.css";
 import SubmitBtn from "../components/SubmitBtn";
+import Loader from "../components/Loader";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [notification, setNotification] = useState("");
+  const [loader, setLoader] = useState(false);
 
   const registerUser = async () => {
+    setLoader(true);
     try {
       const data = {
         username: username,
@@ -22,19 +25,31 @@ export default function RegisterPage() {
       });
       const body = await req.json();
       if (body.status === "error") {
+        setNotification("");
         setError(body.message);
+        setTimeout(() => {
+          setLoader(false);
+        }, 2000);
       } else {
+        setTimeout(() => {
+          setLoader(false);
+        }, 2000);
         setNotification(body.message);
       }
       console.log(body);
     } catch (err) {
       console.log(err);
+      setTimeout(() => {
+        setLoader(false);
+      }, 2000);
+      setNotification("");
       setError("User register failed");
     }
   };
   return (
     <>
       <Header />
+      {loader && <Loader />}
       <main className="main">
         <section className="main__register">
           <form
@@ -43,6 +58,7 @@ export default function RegisterPage() {
             onSubmit={(e) => {
               e.preventDefault();
               registerUser();
+              setLoader(true);
             }}
           >
             <label htmlFor="username">
