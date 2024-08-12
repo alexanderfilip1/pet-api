5c0218574000d7a306e40f71ca42916a55516096
alexanderfilip1 <showgammer@gmail.com>
2024-08-12 13:33:34 +0300

    Implemented upload animals section

--- frontend/src/components/FetchAnimals.jsx
+++ frontend/src/components/FetchAnimals.jsx
@@ -51,10 +51,10 @@ export default function FetchAnimals() {
   return (
     <>
       {loader && <Loader />}
-      <section className="fetchAnimals">
+      <section className="fetchAnimals container">
         <div className="fetchAnimals__wrapper">
           <form
-            className="fetchAnimals__form"
+            className="form"
             onSubmit={(e) => {
               e.preventDefault();
               fetchAnimalData();
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import FetchAnimals from "../components/FetchAnimals";
 import "../assets/css/HomePage.css";
+import UploadAnimals from "../components/UploadAnimals";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <div className="wrapper">
         <main className="main" style={{ width: "100%", position: "initial" }}>
           <FetchAnimals />
+          <UploadAnimals />
         </main>
       </div>
     </>
