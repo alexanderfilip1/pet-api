03a0eca1f1c11031b01ed2fd7b1a288fe750c4d6
alexanderfilip1 <showgammer@gmail.com>
2024-08-13 19:50:22 +0300

    Updated animal schema to make  field optional

--- backend/schemas/animalSchema.js
+++ backend/schemas/animalSchema.js
@@ -8,7 +8,10 @@ const animalSchema = z.object({
     .refine((value) => value === 0 || value === 1, {
       message: "Random must be either 0 or 1",
     }),
-  breed: z.string().min(3, "Breed must be at least 3 characters long"),
+  breed: z
+    .string()
+    .min(3, "Breed must be at least 3 characters long")
+    .optional(),
 });
 
 module.exports = animalSchema;
